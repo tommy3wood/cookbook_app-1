a92f43ff9e7f3b9802d0ad3cc9cf2b90b916864a
JackMarx <seriousfools@gmail.com>
2020-01-17 09:40:09 -0600

    index and show

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,11 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.last
-    render "one_recipe_view.json.jb"
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jb'
   end
 end
