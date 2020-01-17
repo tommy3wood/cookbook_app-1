763d296e0da882dfb2fea138668d461e2fdf73fe
JackMarx <seriousfools@gmail.com>
2020-01-17 10:26:43 -0600

    create

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -4,6 +4,18 @@ class Api::RecipesController < ApplicationController
     render 'index.json.jb'
   end
 
+  def create
+    @recipe = Recipe.new(
+                         title: params[:title],
+                         chef: params[:chef],
+                         ingredients: params[:ingredients],
+                         directions: params[:directions],
+                         prep_time: params[:prep_time]
+                        )
+    @recipe.save
+    render "show.json.jb"
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     render 'show.json.jb'
