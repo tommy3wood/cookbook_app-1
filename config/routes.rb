f152f0d5722924622fbc02fad48e989ddea03678
JackMarx <seriousfools@gmail.com>
2020-01-27 12:29:01 -0600

    authentication part 1 & 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do 
+    post '/users' => 'users#create'
+
+    post "/sessions" => "sessions#create"
+
     get '/recipes' => 'recipes#index'
     post '/recipes' => 'recipes#create'
     get '/recipes/:id' => 'recipes#show'
     patch '/recipes/:id' => 'recipes#update'
     delete '/recipes/:id' => 'recipes#destroy'
+
+    
   end
 end
