c78539bbc8e9a88e33fdf9b7cb50e59495724897
tommy3wood <thomaswcurran1014@gmail.com>
2020-03-05 14:03:34 -0600

    prepped for heroku

--- db/old_seeds_2.rb
+++ db/old_seeds_2.rb
@@ -1,7 +1,6 @@
 User.create!([
   {name: "Joe Cool", email: "joe@gmail.com", password: "password", password_confirmation: "password"},
-  {name: "Suzy Q", email: "sue@gmail.com", password: "password", password_confirmation: "password"},
-  {name: "tom", email: "tom@tom.com", password: "password", password_confirmation: "password"}
+  {name: "Suzy Q", email: "sue@gmail.com", password: "password", password_confirmation: "password"}
 ])
 Recipe.create!([
   {title: "Fava Bean side dish", ingredients: "ice, tap water, 2 lbs fresh fava beans in the pod, 1 tablespoon butter, 1 teaspoon olive oil, 2 garlic cloves", directions: "boil 4 cups of water, add salt, place fava beans in the boiling water for 5 min, remove fava beans and place in ice water, add seasoning to taste", image_url: "http://bijouxs.com/wp-content/uploads/2011/05/Favabordercropsmall.jpg", prep_time: 125, user_id: 2},
@@ -14,4 +13,3 @@ Recipe.create!([
   {title: "spam", ingredients: "can, key, meat", directions: "twist key, open can, enjoy", image_url: "https://www.spam.com/images/5/f/5/5f57337cd256de4e1ad3bb9d1c8409e1c1ec8bc2/spam-classic.png", prep_time: 14, user_id: 2},
   {title: "Maple Simple Syr", ingredients: "tree, tap", directions: "tap the sap, collect bucket", image_url: "https://cdn.shopify.com/s/files/1/0262/3940/2077/products/macs-maple-syrup-glass-leaf-bottle-bag-3_1200x1200.jpg?v=1565371649", prep_time: 360, user_id: 2}
 ])
-
